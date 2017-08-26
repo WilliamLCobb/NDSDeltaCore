ac969e741de441c7822b992f2b77d1b73d341da6
Will Cobb <willlcobb@gmail.com>
2017-08-26 10:38:33 -0500

    Updated to new core format

--- NDSDeltaCore/NDS.swift
+++ NDSDeltaCore/NDS.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+import AVFoundation
+
+import DeltaCore
 
 public extension GameType
 {
@@ -17,13 +20,26 @@ public struct NDS: DeltaCoreProtocol
 {
     public static let core = NDS()
     
-    public let bundleIdentifier: String = "com.rileytestut.NDSDeltaCore"
+    public let gameType = GameType.nds
     
-    public let supportedGameTypes: Set<GameType> = [.nds]
+    public let bundleIdentifier = "com.rileytestut.NDSDeltaCore"
+    
+    public let gameSaveFileExtension = "sav"
+    
+    public let frameDuration = (1.0 / 60.0)
+    
+    public let audioFormat = AVAudioFormat(commonFormat: .pcmFormatInt16, sampleRate: 32768, channels: 2, interleaved: true)
+    
+    public let videoFormat = VideoFormat(pixelFormat: .abgr1555, dimensions: CGSize(width: 256, height: 384))
+    
+    public let supportedCheatFormats: Set<CheatFormat> = {
+        let actionReplayFormat = CheatFormat(name: NSLocalizedString("Action Replay", comment: ""), format: "XXXXXXXX YYYYYYYY", type: .actionReplay)
+        let gameSharkFormat = CheatFormat(name: NSLocalizedString("GameShark", comment: ""), format: "XXXXXXXX YYYYYYYY", type: .gameShark)
+        let codeBreakerFormat = CheatFormat(name: NSLocalizedString("Code Breaker", comment: ""), format: "XXXXXXXX YYYY", type: .codeBreaker)
+        return [actionReplayFormat, gameSharkFormat, codeBreakerFormat]
+    }()
     
     public let emulatorBridge: EmulatorBridging = NDSEmulatorBridge.shared
-    
-    public let emulatorConfiguration: EmulatorConfiguration = NDSEmulatorConfiguration()
     
     public let inputTransformer: InputTransforming = NDSInputTransformer()
     
